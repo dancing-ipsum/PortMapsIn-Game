f5a688b717a0aaff31f13bd4b9ebc2055ca0377f
MCPEGamerJPatGitHub <MCPEGamerJPatGitHub@users.noreply.github.com>
2015-05-07 16:22:19 +0400

    Update main.php

--- src/PortMapsIn-Game/main.php
+++ src/PortMapsIn-Game/main.php
@@ -1,3 +1,13 @@
 <?php
 
-namespace MCPEGamerJP/PortMapsIn-Game
+namespace MCPEGamerJP\PortMapsIn-Game
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\tile\sign;
+use pocketmine\event\block\signchangeevent;
+use pocketmine\event\player\PlayerInteractEvent;
+use pocketmine\worlds\enableworld;
+
+       public $plugins;
+       
+class Main extends PluginBase
